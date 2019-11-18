091b22054b4688b555b312feaaa13d6b7b8e4e3a
jan <declercqjan@gmail.com>
2019-11-18 11:45:35 +0100

    forecast voor gent volgende 5 dagen in console. einde test

--- Example/09-fetch/01-list-to-console/script.js
+++ Example/09-fetch/01-list-to-console/script.js
@@ -21,13 +21,24 @@
 // Run live preview of index.html and check console when clicking on button
 
 async function test() {
-    // await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=fac9676aa8de6252977e1a8672e861e2")
-    // await fetch("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
-    await fetch("https://api.openweathermap.org/data/2.5/weather?q=Gent,be&appid=fac9676aa8de6252977e1a8672e861e2")
-    .then(ste => ste.json())
+    // standaarddata voor moskou await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=fac9676aa8de6252977e1a8672e861e2")
+    // example await fetch("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
+   //  gent await fetch("https://api.openweathermap.org/data/2.5/weather?q=Gent,be&appid=fac9676aa8de6252977e1a8672e861e2")
+   await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Gent,be&appid=fac9676aa8de6252977e1a8672e861e2")
+   .then(ste => ste.json())
     .then(result => {
         console.log(result)
-        console.log(result.city)
+        // console.log(result.city)
+        // console.log(result.list)
+        // console.log(result.list.weather);
+        result.list.forEach(element => {
+            // console.log(element);
+            console.log(element.weather)
+            console.log(element.weather[0]);
+            console.log(element.weather[0].main);
+            console.log(element.weather[0].description);
+        });
+
     })
 }
 document.getElementById("run").addEventListener("click",test)
