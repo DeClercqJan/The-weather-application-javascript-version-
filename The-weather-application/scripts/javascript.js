4f0dcfa6bee82f6f939fb3ee9d46ffad00f9c9a4
jan <declercqjan@gmail.com>
2019-11-19 15:48:26 +0100

    added weekday data

--- The-weather-application/scripts/javascript.js
+++ The-weather-application/scripts/javascript.js
@@ -1,38 +1,72 @@
 forecast_array = [];
 
 function change_table() {
-  console.log("test change table");
+  //console.log("test change table");
   table = document.getElementsByTagName("table");
-  console.log(table);
+  //console.log(table);
   table_html = table[0].innerHTML;
-  console.log(table_html);
+  //console.log(table_html);
   weather_array = [];
   date_array = [];
+  element_weekday_array = [];
   forecast_array.forEach(element => {
     // console.log(element);
     // console.log(element.weather[0].description);
     // console.log(element.dt_txt);
     if (element.dt_txt.includes("12:00:00")) {
-      console.log(element);
+      //console.log(element);
       // console.log(element.dt_txt);
-      console.log(element.weather[0].description);
+      //console.log(element.weather[0].description);
       weather = element.weather[0].description;
       weather_array.push(weather);
       date = element.dt_txt.split(" ");
-      console.log(date[0]);
+      //console.log(date[0]);
       date_array.push(date[0]);
+      // try to get weekday
+      console.log(date[0]);
+      date_date = new Date(date[0]);
+      console.log(date_date);
+      date_weekday_number = date_date.getDay();
+      element_weekday = "";
+      switch (date_weekday_number) {
+        case 1:
+          element_weekday = "Monday";
+          break;
+        case 2:
+          element_weekday = "Tuesday";
+          break;
+        case 3:
+          element_weekday = "Wednesday";
+          break;
+        case 4:
+          element_weekday = "Thursday";
+          break;
+        case 5:
+          element_weekday = "Friday";
+          break;
+        case 6:
+          element_weekday = "Saturday";
+          break;
+        case 0:
+          element_weekday = "Sunday";
+          break;
+      }
+      console.log(element_weekday);
+      element_weekday_array.push(element_weekday);
     }
-    console.log(date_array);
-    console.log(weather_array);
+    //console.log(date_array);
+    //console.log(weather_array);
     for (i = 0; i < date_array.length; i++) {
       // for needs to be 1
       document.getElementById(
         `table_row_${i + 1}`
       ).innerHTML = `<td>${date_array[i]}</td>
+      <td>${element_weekday_array[i]}</td>
   <td>${weather_array[i]}</td>`;
     }
   });
-  console.log(all_data.city.name);
+  console.log(element_weekday_array);
+  //console.log(all_data.city.name);
   document.getElementById("town").innerHTML = all_data.city.name;
 }
 
@@ -42,15 +76,13 @@ function isFloat(x) {
 
 async function fetch_coordinates() {
   // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
-  console.log(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${my_position_latitude}&lon=${my_position_longitude}&appid=fac9676aa8de6252977e1a8672e861e2`
-  );
+  //console.log(  `https://api.openweathermap.org/data/2.5/forecast?lat=${my_position_latitude}&lon=${my_position_longitude}&appid=fac9676aa8de6252977e1a8672e861e2`);
   await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${my_position_latitude}&lon=${my_position_longitude}&appid=fac9676aa8de6252977e1a8672e861e2`
   )
     .then(ste => ste.json())
     .then(result => {
-      console.log(result);
+      //console.log(result);
       all_data = result;
       forecast_array = result.list;
       // console.log(result.city)
@@ -60,10 +92,10 @@ async function fetch_coordinates() {
         
         result.list.forEach(element => {
             // console.log(element);
-            console.log(element.weather)
-            console.log(element.weather[0]);
-            console.log(element.weather[0].main);
-            console.log(element.weather[0].description);
+            //console.log(element.weather)
+            //console.log(element.weather[0]);
+            //console.log(element.weather[0].main);
+            //console.log(element.weather[0].description);
         });
 
 
@@ -77,7 +109,7 @@ function showPosition() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       // getting "is not a float"-error from API
-      console.log(position);
+      //console.log(position);
       my_position_longitude = position.coords.longitude;
       my_position_latitude = position.coords.latitude;
       fetch_coordinates();
@@ -97,16 +129,16 @@ async function fetch_input() {
  await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Gent,be&appid=fac9676aa8de6252977e1a8672e861e2")
  .then(ste => ste.json())
   .then(result => {
-      console.log(result)
+      //console.log(result)
       // console.log(result.city)
       // console.log(result.list)
       // console.log(result.list.weather);
       result.list.forEach(element => {
           // console.log(element);
-          console.log(element.weather)
-          console.log(element.weather[0]);
-          console.log(element.weather[0].main);
-          console.log(element.weather[0].description);
+          //console.log(element.weather)
+          //console.log(element.weather[0]);
+          //console.log(element.weather[0].main);
+          //console.log(element.weather[0].description);
       });
 
   })
@@ -120,16 +152,16 @@ async function fetch_input() {
   // var country = "BE";
   var country_input = document.getElementById("input_field_country").value;
   var country = country_input.toUpperCase();
-  console.log(country);
+  //console.log(country);
   var link = "https://api.openweathermap.org/data/2.5/forecast/?q=";
   var key = "fac9676aa8de6252977e1a8672e861e2";
 
   // THIJS: AWAIT DOESN'T DO ANYTHING: FETCH ALONE + THEN / OR: USE CONST VARIABLE WITH AWAIT FETCH (PROMISE?) . otherwise? code would run without making sense??
-  fetch(link + city + "," + country + "&appid=" + key)
+  await fetch(link + city + "," + country + "&appid=" + key)
     .then(element => element.json())
     .then(element => {
       all_data = element;
-      console.log(element);
+      //console.log(element);
       // console.log(result.city)
       // console.log(result.list)
       // console.log(result.list.weather);
